049d6c9836fecca42e1e9af1445ec05cfbcd46eb
Vadim <aonyko@gmail.com>
2026-07-04 14:16:12 +0300

    com init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        int health = 100;
-        int damage = 35;
+        int count = 0;
+        int sum = 0;
 
-        System.out.println("Player's Health: " + health + " ,monster kick player and take a damage: " + damage + "\nPlayer's Health: " +
-                (health - damage) );
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i + ")i = " + i);
+        }
+        System.out.println("==========NEXT==========");
+        for (int i = 0; i <= 20; i+=2) {
+            System.out.println(count++ + ")i = " + i);
+        }
+        count = 0;
+        System.out.println("==========NEXT==========");
+        for (int i = 0; i <= 5; i++) {
+            sum += i;
+            System.out.println("Summary :" + sum);
+        }
     }
 }
