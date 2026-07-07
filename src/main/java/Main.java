60bcef21af175b47007e257be9501cb2ee1de622
Vadim <aonyko@gmail.com>
2026-07-07 13:23:12 +0300

    com init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 4; i++) {
-            for (int j = 1; j <= 8; j++) {
 
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-        for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print(j);
-            }
-            System.out.println();
-        }
-        for (int i = 5; i >= 1; i--) {
-            for (int j = 5; j >= i; j--) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+    }
+    public static void attack () {
+        System.out.println("Player attacked enemy");
+    }
+    public static String attack (String name) {
+        return "Player attacked" + name;
+    }
+    public static void attack (String name , int damage) {
+        System.out.println("Player attacked" + name + " " + damage + " damage");
     }
 }
+
+
+
